ac727eb3b6d3707e48b5ed9699f8dcf4e5e92686
Jamie Wood <jamie.wood70@ntlworld.com>
2016-01-27 20:52:15 +0000

    Replace tabs with spaces

--- src/main/java/uk/ac/cam/november/simulation/DataGenerator.java
+++ src/main/java/uk/ac/cam/november/simulation/DataGenerator.java
@@ -7,169 +7,169 @@ import com.google.gson.JsonObject;
 
 public class DataGenerator {
 
-	/*
-	 * Example JSON output (from CAN Boat using sample data on Wiki)
-	 * 
-	 * {"timestamp":"2011-11-24-22:42:04.388","prio":2,"src":36,"dst":255,"pgn":127251,"description":"Rate of Turn","fields":{"SID":125,"Rate":0.029200}}
-	 * {"timestamp":"2011-11-24-22:42:04.390","prio":2,"src":36,"dst":255,"pgn":127250,"description":"Vessel Heading","fields":{"SID":0,"Heading":182.4,"Deviation":0.0,"Variation":0.0,"Reference":"Magnetic"}}
-	 * {"timestamp":"2011-11-24-22:42:04.437","prio":2,"src":36,"dst":255,"pgn":130306,"description":"Wind Data","fields":{"SID":177,"Wind Speed":0.92,"Wind Angle":353.4,"Reference":"Apparent"}}
-	 * 
-	 */
-	
-	/** Priority for generated messages. 0 is highest priority, 7 is lowest. */
-	public static int DEFAULT_PRIORITY = 2;
-	/** Arbitrary source address for the generated packets. */
-	public static int DEFAULT_SRC = 36;
-	/** Destination of 255 means broadcast to all devices on NMEA 2000 network. */
-	public static int DEFAULT_DEST = 255;
-	
-	/**
-	 * Creates a blank packet containing the essential fields such as timestamp, priority, source and destination.
-	 * Default values are:<ul>
-	 * <li>timestamp: current time and date</li>
-	 * <li>priority: 2</li>
-	 * <li>source: 36</li>
-	 * <li>destination: 255 (broadcast)</li>
-	 * </ol>
-	 * Also creates the {@code fields} array ready to be populated with data.
-	 * @return  {@code JsonObject} containing the packet
-	 */
-	public static JsonObject createDefaultPacket() {
-		JsonObject packet = new JsonObject();
-		
-		SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss.SSS");
-		
-		packet.addProperty("timestamp", timestampFormat.format(new Date()));
-		packet.addProperty("prio", DEFAULT_PRIORITY);
-		packet.addProperty("src", DEFAULT_SRC);
-		packet.addProperty("dst", DEFAULT_DEST);
-		
-		packet.add("fields", new JsonObject());
-		
-		return packet;
-	}
-	
-	/**
-	 * Creates and returns a vesselHeading packet with the specified heading, 0 degrees deviation, 0 degrees variation.
-	 * @param heading  compass heading in degrees from North
-	 * @return         A {@code JsonObject} containing the packet
-	 * @see            {@link #generateVesselHeadingPacket(double, double, double)}
-	 */
-	public static JsonObject generateVesselHeadingPacket(double heading) {
-		return generateVesselHeadingPacket(heading, 0.0, 0.0);
-	}
-	
-	/**
-	 * Creates and returns a vesselHeading packet with the specified heading, deviation and variation.
-	 * See <a href="http://www.cockpitcards.co.uk/page15.htm">this</a> webpage for definitions.
-	 * @param heading    Compass heading from North (degrees)	
-	 * @param deviation  Magnetic deviation from true value caused by ferrous metal on vessel (degrees)
-	 * @param variation  Difference from Magnetic North to True North (degrees)
-	 * @return           A {@code JsonObject} containing the packet
-	 */
-	public static JsonObject generateVesselHeadingPacket(double heading, double deviation, double variation) {
-		if ( heading < 0.0 || heading > 360.0) {
-			throw new IllegalArgumentException("Heading value must be between 0 and 360 degrees");
-		}
-		if ( deviation < -180.0 || deviation > 180.0) {
-			throw new IllegalArgumentException("Deviation value must be between -180 and 180 degrees");
-		}
-		if ( variation < -180.0 || variation > 180.0) {
-			throw new IllegalArgumentException("Variation value must be between -180 and 180 degrees");
-		}
-		
-		JsonObject packet = createDefaultPacket();
-		
-		packet.addProperty("pgn", 127250);
-		packet.addProperty("description", "Vessel Heading");
-		
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		fields.addProperty("SID", 0);
-		fields.addProperty("Heading", heading);
-		fields.addProperty("Deviation", deviation);
-		fields.addProperty("Variation", variation);
-		fields.addProperty("Reference", "Magnetic");
-		
-		return packet;
-	}
-	
-	/**
-	 * Creates and returns a waterDepth packet with the specified water depth and zero offset.
-	 * @param waterDepth  Depth of water below the sensor (meters)
-	 * @param offset      Distance from sensor to surface (positive) or keel (negative)
-	 * @return            A {@code JsonObject} containing the packet
-	 */
-	public static JsonObject generateWaterDepthPacket(double waterDepth, double offset) {
-		if ( waterDepth < 0.0 ) {
-			throw new IllegalArgumentException("Water depth value must be >= 0 meters");
-		}
-		
-		JsonObject packet = createDefaultPacket();
-		
-		packet.addProperty("pgn", 128267);
-		packet.addProperty("description", "Water Depth");
-		
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		fields.addProperty("SID", 0);
-		fields.addProperty("Depth", waterDepth);
-		fields.addProperty("Offset", offset);
-		
-		return packet;
-	}
-	
-	/**
-	 * Creates and returns a windData packet with the specified wind speed and wind angle.
-	 * @param windSpeed  Speed of wind (meters per second)
-	 * @param windAngle  Angle of wind (degrees, clockwise from bow)
-	 * @return           A {@code JsonObject} containing the packet
-	 */
-	public static JsonObject generateWindDataPacket(double windSpeed, double windAngle){
-		if ( windSpeed < 0.0 ) {
-			throw new IllegalArgumentException("Wind speed value must be >= 0 m/s");
-		}
-		if ( windAngle < 0.0 || windAngle > 360.0) {
-			throw new IllegalArgumentException("Wind angle value must be between 0 and 360 degrees");
-		}
-		
-		JsonObject packet = createDefaultPacket();
-		
-		packet.addProperty("pgn", 130306);
-		packet.addProperty("description", "Wind Data");
-		
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		fields.addProperty("SID", 0);
-		fields.addProperty("Wind Speed", windSpeed);
-		fields.addProperty("Wind Angle", windAngle);
-		fields.addProperty("Reference", "Apparent");
-		
-		return packet;
-	}
-	
-	/**
-	 * Creates and returns a speed packet with the specified speed <b>relative to water</b>.
-	 * @param vesselSpeed  Speed of vessel relative to water (meters per second)
-	 * @return             A {@code JsonObject} containing the packet
-	 */
-	public static JsonObject generateSpeedPacket(double vesselSpeed){
-		if ( vesselSpeed < 0.0 ) {
-			throw new IllegalArgumentException("Vessel speed value must be >= 0 m/s");
-		}
-		
-		JsonObject packet = createDefaultPacket();
-		
-		packet.addProperty("pgn", 128259);
-		packet.addProperty("description", "Speed");
-		
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		fields.addProperty("SID", 0);
-		fields.addProperty("Speed Water Referenced", vesselSpeed);
-		fields.addProperty("Speed Water Referenced Type", "Paddle wheel");
-		
-		return packet;
-	}
-	
+    /*
+     * Example JSON output (from CAN Boat using sample data on Wiki)
+     * 
+     * {"timestamp":"2011-11-24-22:42:04.388","prio":2,"src":36,"dst":255,"pgn":127251,"description":"Rate of Turn","fields":{"SID":125,"Rate":0.029200}}
+     * {"timestamp":"2011-11-24-22:42:04.390","prio":2,"src":36,"dst":255,"pgn":127250,"description":"Vessel Heading","fields":{"SID":0,"Heading":182.4,"Deviation":0.0,"Variation":0.0,"Reference":"Magnetic"}}
+     * {"timestamp":"2011-11-24-22:42:04.437","prio":2,"src":36,"dst":255,"pgn":130306,"description":"Wind Data","fields":{"SID":177,"Wind Speed":0.92,"Wind Angle":353.4,"Reference":"Apparent"}}
+     * 
+     */
+    
+    /** Priority for generated messages. 0 is highest priority, 7 is lowest. */
+    public static int DEFAULT_PRIORITY = 2;
+    /** Arbitrary source address for the generated packets. */
+    public static int DEFAULT_SRC = 36;
+    /** Destination of 255 means broadcast to all devices on NMEA 2000 network. */
+    public static int DEFAULT_DEST = 255;
+    
+    /**
+     * Creates a blank packet containing the essential fields such as timestamp, priority, source and destination.
+     * Default values are:<ul>
+     * <li>timestamp: current time and date</li>
+     * <li>priority: 2</li>
+     * <li>source: 36</li>
+     * <li>destination: 255 (broadcast)</li>
+     * </ol>
+     * Also creates the {@code fields} array ready to be populated with data.
+     * @return  {@code JsonObject} containing the packet
+     */
+    public static JsonObject createDefaultPacket() {
+        JsonObject packet = new JsonObject();
+        
+        SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss.SSS");
+        
+        packet.addProperty("timestamp", timestampFormat.format(new Date()));
+        packet.addProperty("prio", DEFAULT_PRIORITY);
+        packet.addProperty("src", DEFAULT_SRC);
+        packet.addProperty("dst", DEFAULT_DEST);
+        
+        packet.add("fields", new JsonObject());
+        
+        return packet;
+    }
+    
+    /**
+     * Creates and returns a vesselHeading packet with the specified heading, 0 degrees deviation, 0 degrees variation.
+     * @param heading  compass heading in degrees from North
+     * @return         A {@code JsonObject} containing the packet
+     * @see            {@link #generateVesselHeadingPacket(double, double, double)}
+     */
+    public static JsonObject generateVesselHeadingPacket(double heading) {
+        return generateVesselHeadingPacket(heading, 0.0, 0.0);
+    }
+    
+    /**
+     * Creates and returns a vesselHeading packet with the specified heading, deviation and variation.
+     * See <a href="http://www.cockpitcards.co.uk/page15.htm">this</a> webpage for definitions.
+     * @param heading    Compass heading from North (degrees)    
+     * @param deviation  Magnetic deviation from true value caused by ferrous metal on vessel (degrees)
+     * @param variation  Difference from Magnetic North to True North (degrees)
+     * @return           A {@code JsonObject} containing the packet
+     */
+    public static JsonObject generateVesselHeadingPacket(double heading, double deviation, double variation) {
+        if ( heading < 0.0 || heading > 360.0) {
+            throw new IllegalArgumentException("Heading value must be between 0 and 360 degrees");
+        }
+        if ( deviation < -180.0 || deviation > 180.0) {
+            throw new IllegalArgumentException("Deviation value must be between -180 and 180 degrees");
+        }
+        if ( variation < -180.0 || variation > 180.0) {
+            throw new IllegalArgumentException("Variation value must be between -180 and 180 degrees");
+        }
+        
+        JsonObject packet = createDefaultPacket();
+        
+        packet.addProperty("pgn", 127250);
+        packet.addProperty("description", "Vessel Heading");
+        
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        fields.addProperty("SID", 0);
+        fields.addProperty("Heading", heading);
+        fields.addProperty("Deviation", deviation);
+        fields.addProperty("Variation", variation);
+        fields.addProperty("Reference", "Magnetic");
+        
+        return packet;
+    }
+    
+    /**
+     * Creates and returns a waterDepth packet with the specified water depth and zero offset.
+     * @param waterDepth  Depth of water below the sensor (meters)
+     * @param offset      Distance from sensor to surface (positive) or keel (negative)
+     * @return            A {@code JsonObject} containing the packet
+     */
+    public static JsonObject generateWaterDepthPacket(double waterDepth, double offset) {
+        if ( waterDepth < 0.0 ) {
+            throw new IllegalArgumentException("Water depth value must be >= 0 meters");
+        }
+        
+        JsonObject packet = createDefaultPacket();
+        
+        packet.addProperty("pgn", 128267);
+        packet.addProperty("description", "Water Depth");
+        
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        fields.addProperty("SID", 0);
+        fields.addProperty("Depth", waterDepth);
+        fields.addProperty("Offset", offset);
+        
+        return packet;
+    }
+    
+    /**
+     * Creates and returns a windData packet with the specified wind speed and wind angle.
+     * @param windSpeed  Speed of wind (meters per second)
+     * @param windAngle  Angle of wind (degrees, clockwise from bow)
+     * @return           A {@code JsonObject} containing the packet
+     */
+    public static JsonObject generateWindDataPacket(double windSpeed, double windAngle){
+        if ( windSpeed < 0.0 ) {
+            throw new IllegalArgumentException("Wind speed value must be >= 0 m/s");
+        }
+        if ( windAngle < 0.0 || windAngle > 360.0) {
+            throw new IllegalArgumentException("Wind angle value must be between 0 and 360 degrees");
+        }
+        
+        JsonObject packet = createDefaultPacket();
+        
+        packet.addProperty("pgn", 130306);
+        packet.addProperty("description", "Wind Data");
+        
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        fields.addProperty("SID", 0);
+        fields.addProperty("Wind Speed", windSpeed);
+        fields.addProperty("Wind Angle", windAngle);
+        fields.addProperty("Reference", "Apparent");
+        
+        return packet;
+    }
+    
+    /**
+     * Creates and returns a speed packet with the specified speed <b>relative to water</b>.
+     * @param vesselSpeed  Speed of vessel relative to water (meters per second)
+     * @return             A {@code JsonObject} containing the packet
+     */
+    public static JsonObject generateSpeedPacket(double vesselSpeed){
+        if ( vesselSpeed < 0.0 ) {
+            throw new IllegalArgumentException("Vessel speed value must be >= 0 m/s");
+        }
+        
+        JsonObject packet = createDefaultPacket();
+        
+        packet.addProperty("pgn", 128259);
+        packet.addProperty("description", "Speed");
+        
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        fields.addProperty("SID", 0);
+        fields.addProperty("Speed Water Referenced", vesselSpeed);
+        fields.addProperty("Speed Water Referenced Type", "Paddle wheel");
+        
+        return packet;
+    }
+    
 }
--- src/test/java/uk/ac/cam/november/simulation/DataGeneratorTest.java
+++ src/test/java/uk/ac/cam/november/simulation/DataGeneratorTest.java
@@ -9,106 +9,106 @@ import org.junit.Test;
 import com.google.gson.JsonObject;
 
 public class DataGeneratorTest {
-	
-	/* createDefaultPacket */
-	
-	@Test
-	public void defaultPacketShouldContainAllFields(){
-		JsonObject packet = DataGenerator.createDefaultPacket();
-		assertTrue(packet.has("timestamp"));
-		assertTrue(packet.has("prio"));
-		assertTrue(packet.has("src"));
-		assertTrue(packet.has("dst"));
-		assertTrue(packet.has("fields"));
-	}
-	
-	@Test
-	public void defaultPacketShouldHaveCorrectDefaultValues(){
-		JsonObject packet = DataGenerator.createDefaultPacket();
-		assertEquals(DataGenerator.DEFAULT_PRIORITY, packet.get("prio").getAsInt());
-		assertEquals(DataGenerator.DEFAULT_SRC, packet.get("src").getAsInt());
-		assertEquals(DataGenerator.DEFAULT_DEST, packet.get("dst").getAsInt());
-		assertEquals(23, packet.get("timestamp").getAsString().length());
-	}
-	
-	/* generateVesselHeadingPacket */
-	
-	@Test
-	public void vesselHeadingPacketsShouldCheckInputRange() {
-		try{ DataGenerator.generateVesselHeadingPacket(-1.0, 0, 0); fail("Heading did not catch <0"); } catch(IllegalArgumentException e) {}
-		try{ DataGenerator.generateVesselHeadingPacket(370, 0, 0); fail("Heading did not catch >360"); } catch(IllegalArgumentException e) {}
-		try{ DataGenerator.generateVesselHeadingPacket(0, -200, 0); fail("Deviation did not catch <-180"); } catch(IllegalArgumentException e) {}
-		try{ DataGenerator.generateVesselHeadingPacket(0, 200, 0); fail("Deviation did not catch >180"); } catch(IllegalArgumentException e) {}
-		try{ DataGenerator.generateVesselHeadingPacket(0, 0, -200); fail("Variation did not catch <-180"); } catch(IllegalArgumentException e) {}
-		try{ DataGenerator.generateVesselHeadingPacket(0, 0, 200); fail("Variation did not catch >180"); } catch(IllegalArgumentException e) {}
-	}
-	
-	@Test
-	public void vesselHeadingPacketShouldContainCorrectFields() {
-		JsonObject packet = DataGenerator.generateVesselHeadingPacket(100.0, 1.0, -1.0);
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		assertEquals(packet.get("pgn").getAsInt(), 127250);
-		
-		assertEquals(fields.get("Heading").getAsDouble(), 100.0, 0.01);
-		assertEquals(fields.get("Deviation").getAsDouble(), 1.0, 0.01);
-		assertEquals(fields.get("Variation").getAsDouble(), -1.0, 0.01);
-	}
-	
-	/* generateWaterDepthPacket */
-	
-	@Test
-	public void waterDepthPacketShouldCheckInputRange() {
-		try{ DataGenerator.generateWaterDepthPacket(-1.0, 0); fail("Water Depth did not catch <0"); } catch(IllegalArgumentException e) {}
-	}
-	
-	@Test
-	public void waterDepthPacketShouldContainCorrectFields() {
-		JsonObject packet = DataGenerator.generateWaterDepthPacket(100.0, 1.0);
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		assertEquals(packet.get("pgn").getAsInt(), 128267);
-		
-		assertEquals(fields.get("Depth").getAsDouble(), 100.0, 0.01);
-		assertEquals(fields.get("Offset").getAsDouble(), 1.0, 0.01);
-	}
-	
-	/* generateWindDataPacket */
-	
-	@Test
-	public void windDepthPacketShouldCheckInputRange() {
-		try{ DataGenerator.generateWindDataPacket(-1.0, 0); fail("Wind Speed did not catch <0"); } catch(IllegalArgumentException e) {}
-		try{ DataGenerator.generateWindDataPacket(1.0, -10.0); fail("Wind Angle did not catch <0"); } catch(IllegalArgumentException e) {}
-		try{ DataGenerator.generateWindDataPacket(1.0, 370.0); fail("Wind Angle did not catch >360"); } catch(IllegalArgumentException e) {}
-	}
-	
-	@Test
-	public void windDataPacketShouldContainCorrectFields() {
-		JsonObject packet = DataGenerator.generateWindDataPacket(100.0, 1.0);
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		assertEquals(packet.get("pgn").getAsInt(), 130306);
-		
-		assertEquals(fields.get("Wind Speed").getAsDouble(), 100.0, 0.01);
-		assertEquals(fields.get("Wind Angle").getAsDouble(), 1.0, 0.01);
-		assertEquals(fields.get("Reference").getAsString(), "Apparent");
-	}
-	
-	/* generateSpeedPacket */
-	
-	@Test
-	public void speedPacketShouldCheckInputRange() {
-		try{ DataGenerator.generateSpeedPacket(-1.0); fail("Vessel speed value did not catch <0"); } catch(IllegalArgumentException e) {}
-	}
-	
-	@Test
-	public void speedPacketShouldContainCorrectFields(){
-		JsonObject packet = DataGenerator.generateSpeedPacket(100.0);
-		JsonObject fields = packet.getAsJsonObject("fields");
-		
-		assertEquals(packet.get("pgn").getAsInt(), 128259);
-		
-		assertEquals(fields.get("Speed Water Referenced").getAsDouble(), 100.0, 0.01);
-	}
-	
+    
+    /* createDefaultPacket */
+    
+    @Test
+    public void defaultPacketShouldContainAllFields(){
+        JsonObject packet = DataGenerator.createDefaultPacket();
+        assertTrue(packet.has("timestamp"));
+        assertTrue(packet.has("prio"));
+        assertTrue(packet.has("src"));
+        assertTrue(packet.has("dst"));
+        assertTrue(packet.has("fields"));
+    }
+    
+    @Test
+    public void defaultPacketShouldHaveCorrectDefaultValues(){
+        JsonObject packet = DataGenerator.createDefaultPacket();
+        assertEquals(DataGenerator.DEFAULT_PRIORITY, packet.get("prio").getAsInt());
+        assertEquals(DataGenerator.DEFAULT_SRC, packet.get("src").getAsInt());
+        assertEquals(DataGenerator.DEFAULT_DEST, packet.get("dst").getAsInt());
+        assertEquals(23, packet.get("timestamp").getAsString().length());
+    }
+    
+    /* generateVesselHeadingPacket */
+    
+    @Test
+    public void vesselHeadingPacketsShouldCheckInputRange() {
+        try{ DataGenerator.generateVesselHeadingPacket(-1.0, 0, 0); fail("Heading did not catch <0"); } catch(IllegalArgumentException e) {}
+        try{ DataGenerator.generateVesselHeadingPacket(370, 0, 0); fail("Heading did not catch >360"); } catch(IllegalArgumentException e) {}
+        try{ DataGenerator.generateVesselHeadingPacket(0, -200, 0); fail("Deviation did not catch <-180"); } catch(IllegalArgumentException e) {}
+        try{ DataGenerator.generateVesselHeadingPacket(0, 200, 0); fail("Deviation did not catch >180"); } catch(IllegalArgumentException e) {}
+        try{ DataGenerator.generateVesselHeadingPacket(0, 0, -200); fail("Variation did not catch <-180"); } catch(IllegalArgumentException e) {}
+        try{ DataGenerator.generateVesselHeadingPacket(0, 0, 200); fail("Variation did not catch >180"); } catch(IllegalArgumentException e) {}
+    }
+    
+    @Test
+    public void vesselHeadingPacketShouldContainCorrectFields() {
+        JsonObject packet = DataGenerator.generateVesselHeadingPacket(100.0, 1.0, -1.0);
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        assertEquals(packet.get("pgn").getAsInt(), 127250);
+        
+        assertEquals(fields.get("Heading").getAsDouble(), 100.0, 0.01);
+        assertEquals(fields.get("Deviation").getAsDouble(), 1.0, 0.01);
+        assertEquals(fields.get("Variation").getAsDouble(), -1.0, 0.01);
+    }
+    
+    /* generateWaterDepthPacket */
+    
+    @Test
+    public void waterDepthPacketShouldCheckInputRange() {
+        try{ DataGenerator.generateWaterDepthPacket(-1.0, 0); fail("Water Depth did not catch <0"); } catch(IllegalArgumentException e) {}
+    }
+    
+    @Test
+    public void waterDepthPacketShouldContainCorrectFields() {
+        JsonObject packet = DataGenerator.generateWaterDepthPacket(100.0, 1.0);
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        assertEquals(packet.get("pgn").getAsInt(), 128267);
+        
+        assertEquals(fields.get("Depth").getAsDouble(), 100.0, 0.01);
+        assertEquals(fields.get("Offset").getAsDouble(), 1.0, 0.01);
+    }
+    
+    /* generateWindDataPacket */
+    
+    @Test
+    public void windDepthPacketShouldCheckInputRange() {
+        try{ DataGenerator.generateWindDataPacket(-1.0, 0); fail("Wind Speed did not catch <0"); } catch(IllegalArgumentException e) {}
+        try{ DataGenerator.generateWindDataPacket(1.0, -10.0); fail("Wind Angle did not catch <0"); } catch(IllegalArgumentException e) {}
+        try{ DataGenerator.generateWindDataPacket(1.0, 370.0); fail("Wind Angle did not catch >360"); } catch(IllegalArgumentException e) {}
+    }
+    
+    @Test
+    public void windDataPacketShouldContainCorrectFields() {
+        JsonObject packet = DataGenerator.generateWindDataPacket(100.0, 1.0);
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        assertEquals(packet.get("pgn").getAsInt(), 130306);
+        
+        assertEquals(fields.get("Wind Speed").getAsDouble(), 100.0, 0.01);
+        assertEquals(fields.get("Wind Angle").getAsDouble(), 1.0, 0.01);
+        assertEquals(fields.get("Reference").getAsString(), "Apparent");
+    }
+    
+    /* generateSpeedPacket */
+    
+    @Test
+    public void speedPacketShouldCheckInputRange() {
+        try{ DataGenerator.generateSpeedPacket(-1.0); fail("Vessel speed value did not catch <0"); } catch(IllegalArgumentException e) {}
+    }
+    
+    @Test
+    public void speedPacketShouldContainCorrectFields(){
+        JsonObject packet = DataGenerator.generateSpeedPacket(100.0);
+        JsonObject fields = packet.getAsJsonObject("fields");
+        
+        assertEquals(packet.get("pgn").getAsInt(), 128259);
+        
+        assertEquals(fields.get("Speed Water Referenced").getAsDouble(), 100.0, 0.01);
+    }
+    
 }
