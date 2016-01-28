cce59a72703b6671fff7b82788c34fa5e16b75b8
Marie Menshova <mariemenshova@dhcp-10-248-111-118.eduroam.wireless.private.cam.ac.uk>
2016-01-28 14:40:44 +0000

    messages added

---
+++ src/main/java/uk/ac/cam/november/message/WaterDepthFields.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.november.message;
+
+public class WaterDepthFields extends Fields{
+    private float Depth;
+    private float Offset;
+    
+    public float getDepth() {
+        return Depth;
+    }
+    public float getOffset() {
+        return Offset;
+    }
+}
