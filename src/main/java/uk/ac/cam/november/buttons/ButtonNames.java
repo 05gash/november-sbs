eafd59d7fdc0118fdc2c2d39c8eb953ad29a1494
mercury0114 <m.latinis@gmail.com>
2016-01-28 15:34:16 +0000

    tts and buttons integration

---
+++ src/main/java/uk/ac/cam/november/buttons/ButtonNames.java
@@ -0,0 +1,19 @@
+// This is a file that contains the name of every button.
+// Classes relying on button names should import them from 
+// this file rather than to use custom names.
+
+package uk.ac.cam.november.buttons;
+
+class ButtonNames {
+	// When adding a new name, make sure that it is different from all others!
+	public final static String WATER_DEPTH = "WaterDepth";
+	public final static String WIND_SPEED = "WindSpeed";
+}
+
+
+
+
+
+
+
+
---
+++ src/main/java/uk/ac/cam/november/buttons/ButtonsListener.java
@@ -0,0 +1,41 @@
+// This is the main class of the whole package
+// For every button connected to the Raspberry Pi,
+// an GpioPinDigitalInput object is created.
+// Each object waits for a button to be clicked,
+// and calls the PinListener to deal with the click.
+
+package uk.ac.cam.november.buttons;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.RaspiPin;
+
+class ButtonsListener {
+
+	private final static int NUMBER_OF_BUTTONS = 50;  // The actual number is much smaller
+	private final GpioPinDigitalInput buttons[] = new GpioPinDigitalInput[NUMBER_OF_BUTTONS];
+	private final GpioController gpio = GpioFactory.getInstance();
+
+	public ButtonsListener() {
+		buttons[7] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07,
+			PinPullResistance.PULL_DOWN);
+		buttons[7].addListener(new PinListener(ButtonNames.WATER_DEPTH));
+
+		buttons[0] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00,
+			PinPullResistance.PULL_DOWN);
+		buttons[0].addListener(new PinListener(ButtonNames.WIND_SPEED));
+	}
+	
+	// TODO(ml693): main function is just for debugging,
+	// need to remove later
+	public static void main(String args[]) throws InterruptedException {
+		System.out.println("Started listen to the buttons");
+		ButtonsListener buttonsListener = new ButtonsListener();        	
+		for (;;) {
+            		Thread.sleep(500);
+        	}
+	}
+
+}
