a7929405084568aeab0b754bb9a8a7c8583f918b
JMW95 <jamie.wood70@ntlworld.com>
2016-02-01 14:53:50 +0000

    Add WorldModel and tests

---
+++ src/main/java/uk/ac/cam/november/simulation/WorldModel.java
@@ -0,0 +1,125 @@
+package uk.ac.cam.november.simulation;
+
+public class WorldModel {
+
+    public static final float MAX_BOAT_SPEED = 25;
+    public static final float MAX_WIND_SPEED = 100;
+    public static final float MAX_WATER_DEPTH = 250;
+
+    private float heading = 0f;
+    private float boatSpeed = 0f;
+    private float windSpeed = 0f;
+    private float windAngle = 0f;
+    private float waterDepth = 0f;
+
+    /**
+     * Set the heading of the boat in the world model.
+     * @param h Compass heading of the boat in degrees. Value is wrapped into the range 0-360.
+     */
+    public void setHeading(float h) {
+        while(h < 0){
+            h += 360;
+        }
+        while(h > 360){
+            h -= 360;
+        }
+        heading = h;
+    }
+
+    /**
+     * Get the current heading of the boat in the world model.
+     * @return Current compass heading in degrees, in the range 0-360.
+     */
+    public float getHeading() {
+        return heading;
+    }
+
+    /**
+     * Set the boat speed in the world model.
+     * @param s Boat speed in meters-per-second. Value is truncated to the range 0-{@value #MAX_BOAT_SPEED}.
+     */
+    public void setBoatSpeed(float s) {
+        if (s < 0) {
+            s = 0;
+        }
+        if (s > MAX_BOAT_SPEED) {
+            s = MAX_BOAT_SPEED;
+        }
+        boatSpeed = s;
+    }
+
+    /**
+     * Get the current boat speed in the world model.
+     * @return Current boat speed in meters-per-second.
+     */
+    public float getBoatSpeed() {
+        return boatSpeed;
+    }
+    
+    /**
+     * Set the wind speed in the world model.
+     * @param s Wind speed in meters-per-second. Value is truncated into the range 0-{@value #MAX_WIND_SPEED}.
+     */
+    public void setWindSpeed(float s) {
+        if (s < 0) {
+            s = 0;
+        }
+        if (s > MAX_WIND_SPEED) {
+            s = MAX_WIND_SPEED;
+        }
+        windSpeed = s;
+    }
+
+    /**
+     * Get the current wind speed in the world model.
+     * @return Current wind speed in meters-per-second.
+     */
+    public float getWindSpeed() {
+        return windSpeed;
+    }
+
+    /**
+     * Set the wind angle in the world model.
+     * @param a Wind angle in degrees. Value is wrapped into the range 0-360.
+     */
+    public void setWindAngle(float a) {
+        while(a < 0){
+            a += 360;
+        }
+        while(a > 360){
+            a -= 360;
+        }
+        windAngle = a;
+    }
+
+    /**
+     * Get the current wind angle in the world model.
+     * @return Current wind angle in degrees, in the range 0-360.
+     */
+    public float getWindAngle() {
+        return windAngle;
+    }
+    
+    /**
+     * Set the water depth in the world model.
+     * @param d Water depth in meters. Value is truncated into the range 0-{@value #MAX_WATER_DEPTH}.
+     */
+    public void setWaterDepth(float d) {
+        if (d < 0) {
+            d = 0;
+        }
+        if (d > MAX_WATER_DEPTH) {
+            d = MAX_WATER_DEPTH;
+        }
+        waterDepth = d;
+    }
+    
+    /**
+     * Get the current water depth in the world model.
+     * @return Current water depth in meters.
+     */
+    public float getWaterDepth(){
+        return waterDepth;
+    }
+
+}
---
+++ src/test/java/uk/ac/cam/november/simulation/WorldModelTest.java
@@ -0,0 +1,64 @@
+package uk.ac.cam.november.simulation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WorldModelTest {
+
+    @Test
+    public void worldModelShouldDeaultToZeroValues() {
+        WorldModel wm = new WorldModel();
+        assertEquals(wm.getBoatSpeed(), 0, 0.0001);
+        assertEquals(wm.getHeading(), 0, 0.0001);
+        assertEquals(wm.getWaterDepth(), 0, 0.0001);
+        assertEquals(wm.getWindAngle(), 0, 0.0001);
+        assertEquals(wm.getWindSpeed(), 0, 0.0001);
+    }
+
+    @Test
+    public void worldModelShouldTruncateBoatSpeed() {
+        WorldModel wm = new WorldModel();
+        wm.setBoatSpeed(-10);
+        assertEquals(0, wm.getBoatSpeed(), 0.001);
+        wm.setBoatSpeed(WorldModel.MAX_BOAT_SPEED + 100);
+        assertEquals(WorldModel.MAX_BOAT_SPEED, wm.getBoatSpeed(), 0.001);
+    }
+
+    @Test
+    public void worldModelShouldWrapHeading() {
+        WorldModel wm = new WorldModel();
+        wm.setHeading(-10);
+        assertEquals(350, wm.getHeading(), 0.001);
+        wm.setHeading(400);
+        assertEquals(40, wm.getHeading(), 0.001);
+    }
+
+    @Test
+    public void worldModelShouldTruncateWaterDepth() {
+        WorldModel wm = new WorldModel();
+        wm.setWaterDepth(-10);
+        assertEquals(0, wm.getWaterDepth(), 0.001);
+        wm.setWaterDepth(WorldModel.MAX_WATER_DEPTH + 100);
+        assertEquals(WorldModel.MAX_WATER_DEPTH, wm.getWaterDepth(), 0.001);
+    }
+
+    @Test
+    public void worldModelShouldWrapWindAngle() {
+        WorldModel wm = new WorldModel();
+        wm.setWindAngle(-10);
+        assertEquals(350, wm.getWindAngle(), 0.001);
+        wm.setWindAngle(400);
+        assertEquals(40, wm.getWindAngle(), 0.001);
+    }
+
+    @Test
+    public void worldModelShouldTruncateWindSpeed() {
+        WorldModel wm = new WorldModel();
+        wm.setWindSpeed(-10);
+        assertEquals(0, wm.getWindSpeed(), 0.001);
+        wm.setWindSpeed(WorldModel.MAX_WIND_SPEED + 100);
+        assertEquals(WorldModel.MAX_WIND_SPEED, wm.getWindSpeed(), 0.001);
+    }
+
+}
