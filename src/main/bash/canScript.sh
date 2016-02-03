77fb34571e501b99e19884722820b1a75163f6c0
05gash <05gash@googlemail.com>
2016-02-03 13:48:54 +0000

    included actisense serial in canscript

--- src/main/bash/canScript.sh
+++ src/main/bash/canScript.sh
@@ -5,7 +5,8 @@ case $1 in
 	-d)
 		stdbuf -oL -eL python ~/git/november-sbs/src/main/bash/datagen.py | analyzer -json
 		;;
-	-c)
+	-b)
+		actisense-serial -r /dev/ttyUSB0 | analyzer -json
 		;;
 	*)
 		
