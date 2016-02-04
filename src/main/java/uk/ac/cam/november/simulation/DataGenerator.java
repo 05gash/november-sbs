b891924720ea41c0195df2b23e24ed054de69146
JMW95 <jamie.wood70@ntlworld.com>
2016-02-04 11:35:38 +0000

    Add BoatDataOutputter

--- src/main/java/uk/ac/cam/november/simulation/DataGenerator.java
+++ src/main/java/uk/ac/cam/november/simulation/DataGenerator.java
@@ -8,34 +8,37 @@ import uk.ac.cam.november.message.Message;
 import uk.ac.cam.november.message.WaterDepthFields;
 import uk.ac.cam.november.message.WindDataFields;
 
+/**
+ * This class creates {@code Message} objects which emulate those produced by
+ * the CANBoat input stage of the project. This is useful for testing the
+ * data-parsing module, and for demonstration purposes.
+ * 
+ * @author Jamie Wood
+ *
+ */
 public class DataGenerator {
 
-    /*
-     * Example JSON output (from CAN Boat using sample data on Wiki)
-     * 
-     * {"timestamp":"2011-11-24-22:42:04.388","prio":2,"src":36,"dst":255,"pgn":127251,"description":"Rate of Turn","fields":{"SID":125,"Rate":0.029200}}
-     * {"timestamp":"2011-11-24-22:42:04.390","prio":2,"src":36,"dst":255,"pgn":127250,"description":"Vessel Heading","fields":{"SID":0,"Heading":182.4,"Deviation":0.0,"Variation":0.0,"Reference":"Magnetic"}}
-     * {"timestamp":"2011-11-24-22:42:04.437","prio":2,"src":36,"dst":255,"pgn":130306,"description":"Wind Data","fields":{"SID":177,"Wind Speed":0.92,"Wind Angle":353.4,"Reference":"Apparent"}}
-     * 
-     */
-    
     /** Priority for generated messages. 0 is highest priority, 7 is lowest. */
     public static int DEFAULT_PRIORITY = 2;
     /** Arbitrary source address for the generated packets. */
     public static int DEFAULT_SRC = 36;
-    /** Destination of 255 means broadcast to all devices on NMEA 2000 network. */
-    public static int DEFAULT_DEST = 255;
-    
     /**
-     * Creates a blank packet containing the essential fields such as timestamp, priority, source and destination.
-     * Default values are:<ul>
+     * Destination of 255 means broadcast to all devices on NMEA 2000 network.
+     */
+    public static int DEFAULT_DEST = 255;
+
+    /**
+     * Creates a blank packet containing the essential fields such as timestamp,
+     * priority, source and destination. Default values are:
+     * <ul>
      * <li>timestamp: current time and date</li>
      * <li>priority: 2</li>
      * <li>source: 36</li>
      * <li>destination: 255 (broadcast)</li>
      * </ol>
      * Also creates the {@code fields} array ready to be populated with data.
-     * @return  {@code Message} containing the packet
+     * 
+     * @return {@code Message} containing the packet
      */
     public static Message createDefaultPacket() {
         Message packet = new Message();
@@ -43,139 +46,160 @@ public class DataGenerator {
         packet.setPrio(DEFAULT_PRIORITY);
         packet.setSrc(DEFAULT_SRC);
         packet.setDst(DEFAULT_DEST);
-        
+
         return packet;
     }
-    
+
     /**
-     * Creates and returns a vesselHeading packet with the specified heading, 0 degrees deviation, 0 degrees variation.
-     * @param heading  compass heading in degrees from North
-     * @return         A {@code Message} containing the packet
-     * @see            {@link #generateVesselHeadingPacket(double, double, double)}
+     * Creates and returns a vesselHeading packet with the specified heading, 0
+     * degrees deviation, 0 degrees variation.
+     * 
+     * @param heading
+     *            compass heading in degrees from North
+     * @return A {@code Message} containing the packet
+     * @see {@link #generateVesselHeadingPacket(double, double, double)}
      */
     public static Message generateVesselHeadingPacket(float heading) {
         return generateVesselHeadingPacket(heading, 0.0f, 0.0f);
     }
-    
+
     /**
-     * Creates and returns a vesselHeading packet with the specified heading, deviation and variation.
-     * See <a href="http://www.cockpitcards.co.uk/page15.htm">this</a> webpage for definitions.
-     * @param heading    Compass heading from North (degrees)    
-     * @param deviation  Magnetic deviation from true value caused by ferrous metal on vessel (degrees)
-     * @param variation  Difference from Magnetic North to True North (degrees)
-     * @return           A {@code Message} containing the packet
+     * Creates and returns a vesselHeading packet with the specified heading,
+     * deviation and variation. See
+     * <a href="http://www.cockpitcards.co.uk/page15.htm">this</a> webpage for
+     * definitions.
+     * 
+     * @param heading
+     *            Compass heading from North (degrees)
+     * @param deviation
+     *            Magnetic deviation from true value caused by ferrous metal on
+     *            vessel (degrees)
+     * @param variation
+     *            Difference from Magnetic North to True North (degrees)
+     * @return A {@code Message} containing the packet
      */
     public static Message generateVesselHeadingPacket(float heading, float deviation, float variation) {
-        if ( heading < 0.0 || heading > 360.0) {
+        if (heading < 0.0 || heading > 360.0) {
             throw new IllegalArgumentException("Heading value must be between 0 and 360 degrees");
         }
-        if ( deviation < -180.0 || deviation > 180.0) {
+        if (deviation < -180.0 || deviation > 180.0) {
             throw new IllegalArgumentException("Deviation value must be between -180 and 180 degrees");
         }
-        if ( variation < -180.0 || variation > 180.0) {
+        if (variation < -180.0 || variation > 180.0) {
             throw new IllegalArgumentException("Variation value must be between -180 and 180 degrees");
         }
-        
+
         Message packet = createDefaultPacket();
-        
+
         packet.setPgn(127250);
         packet.setDescription("Vessel Heading");
-        
+
         BoatHeadingFields fields = new BoatHeadingFields();
-        
+
         fields.setSID(0);
         fields.setHeading(heading);
         fields.setDeviation(deviation);
         fields.setVariation(variation);
         fields.setReference("Magnetic");
-        
+
         packet.setFields(fields);
-        
+
         return packet;
     }
-    
+
     /**
-     * Creates and returns a waterDepth packet with the specified water depth and zero offset.
-     * @param waterDepth  Depth of water below the sensor (meters)
-     * @param offset      Distance from sensor to surface (positive) or keel (negative)
-     * @return            A {@code Message} containing the packet
+     * Creates and returns a waterDepth packet with the specified water depth
+     * and zero offset.
+     * 
+     * @param waterDepth
+     *            Depth of water below the sensor (meters)
+     * @param offset
+     *            Distance from sensor to surface (positive) or keel (negative)
+     * @return A {@code Message} containing the packet
      */
     public static Message generateWaterDepthPacket(float waterDepth, float offset) {
-        if ( waterDepth < 0.0 ) {
+        if (waterDepth < 0.0) {
             throw new IllegalArgumentException("Water depth value must be >= 0 meters");
         }
-        
+
         Message packet = createDefaultPacket();
-        
+
         packet.setPgn(128267);
         packet.setDescription("Water Depth");
-        
+
         WaterDepthFields fields = new WaterDepthFields();
-        
+
         fields.setSID(0);
         fields.setDepth(waterDepth);
         fields.setOffset(offset);
-        
+
         packet.setFields(fields);
-        
+
         return packet;
     }
-    
+
     /**
-     * Creates and returns a windData packet with the specified wind speed and wind angle.
-     * @param windSpeed  Speed of wind (meters per second)
-     * @param windAngle  Angle of wind (degrees, clockwise from bow)
-     * @return           A {@code Message} containing the packet
+     * Creates and returns a windData packet with the specified wind speed and
+     * wind angle.
+     * 
+     * @param windSpeed
+     *            Speed of wind (meters per second)
+     * @param windAngle
+     *            Angle of wind (degrees, clockwise from bow)
+     * @return A {@code Message} containing the packet
      */
-    public static Message generateWindDataPacket(float windSpeed, float windAngle){
-        if ( windSpeed < 0.0 ) {
+    public static Message generateWindDataPacket(float windSpeed, float windAngle) {
+        if (windSpeed < 0.0) {
             throw new IllegalArgumentException("Wind speed value must be >= 0 m/s");
         }
-        if ( windAngle < 0.0 || windAngle > 360.0) {
+        if (windAngle < 0.0 || windAngle > 360.0) {
             throw new IllegalArgumentException("Wind angle value must be between 0 and 360 degrees");
         }
-        
+
         Message packet = createDefaultPacket();
-        
+
         packet.setPgn(130306);
         packet.setDescription("Wind Data");
-        
+
         WindDataFields fields = new WindDataFields();
-        
+
         fields.setSID(0);
         fields.setWindSpeed(windSpeed);
         fields.setWindAngle(windAngle);
         fields.setReference("Apparent");
-        
+
         packet.setFields(fields);
-        
+
         return packet;
     }
-    
+
     /**
-     * Creates and returns a speed packet with the specified speed <b>relative to water</b>.
-     * @param vesselSpeed  Speed of vessel relative to water (meters per second)
-     * @return             A {@code Message} containing the packet
+     * Creates and returns a speed packet with the specified speed <b>relative
+     * to water</b>.
+     * 
+     * @param vesselSpeed
+     *            Speed of vessel relative to water (meters per second)
+     * @return A {@code Message} containing the packet
      */
-    public static Message generateSpeedPacket(float vesselSpeed){
-        if ( vesselSpeed < 0.0 ) {
+    public static Message generateSpeedPacket(float vesselSpeed) {
+        if (vesselSpeed < 0.0) {
             throw new IllegalArgumentException("Vessel speed value must be >= 0 m/s");
         }
-        
+
         Message packet = createDefaultPacket();
-        
+
         packet.setPgn(128259);
         packet.setDescription("Speed");
-        
+
         BoatSpeedFields fields = new BoatSpeedFields();
-        
+
         fields.setSID(0);
         fields.setSpeedWaterReferenced(vesselSpeed);
         fields.setSpeedWaterReferencedType("Paddle wheel");
-        
+
         packet.setFields(fields);
-        
+
         return packet;
     }
-    
+
 }
--- src/main/java/uk/ac/cam/november/simulation/WorldModel.java
+++ src/main/java/uk/ac/cam/november/simulation/WorldModel.java
@@ -1,5 +1,12 @@
 package uk.ac.cam.november.simulation;
 
+/**
+ * This class stores the world state used in the simulation. The values stored
+ * are heading, boat speed, wind speed, wind direction and water depth.
+ * 
+ * @author Jamie Wood
+ *
+ */
 public class WorldModel {
 
     public static final float MAX_BOAT_SPEED = 25;
@@ -11,19 +18,22 @@ public class WorldModel {
     private float windSpeed = 0f;
     private float windAngle = 0f;
     private float waterDepth = 0f;
-    
+
     private float boatX;
     private float boatY;
 
     /**
      * Set the heading of the boat in the world model.
-     * @param h Compass heading of the boat in degrees. Value is wrapped into the range 0-360.
+     * 
+     * @param h
+     *            Compass heading of the boat in degrees. Value is wrapped into
+     *            the range 0-360.
      */
     public void setHeading(float h) {
-        while(h < 0){
+        while (h < 0) {
             h += 360;
         }
-        while(h > 360){
+        while (h > 360) {
             h -= 360;
         }
         heading = h;
@@ -31,6 +41,7 @@ public class WorldModel {
 
     /**
      * Get the current heading of the boat in the world model.
+     * 
      * @return Current compass heading in degrees, in the range 0-360.
      */
     public float getHeading() {
@@ -39,7 +50,10 @@ public class WorldModel {
 
     /**
      * Set the boat speed in the world model.
-     * @param s Boat speed in meters-per-second. Value is truncated to the range 0-{@value #MAX_BOAT_SPEED}.
+     * 
+     * @param s
+     *            Boat speed in meters-per-second. Value is truncated to the
+     *            range 0-{@value #MAX_BOAT_SPEED}.
      */
     public void setBoatSpeed(float s) {
         if (s < 0) {
@@ -53,15 +67,19 @@ public class WorldModel {
 
     /**
      * Get the current boat speed in the world model.
+     * 
      * @return Current boat speed in meters-per-second.
      */
     public float getBoatSpeed() {
         return boatSpeed;
     }
-    
+
     /**
      * Set the wind speed in the world model.
-     * @param s Wind speed in meters-per-second. Value is truncated into the range 0-{@value #MAX_WIND_SPEED}.
+     * 
+     * @param s
+     *            Wind speed in meters-per-second. Value is truncated into the
+     *            range 0-{@value #MAX_WIND_SPEED}.
      */
     public void setWindSpeed(float s) {
         if (s < 0) {
@@ -75,6 +93,7 @@ public class WorldModel {
 
     /**
      * Get the current wind speed in the world model.
+     * 
      * @return Current wind speed in meters-per-second.
      */
     public float getWindSpeed() {
@@ -83,13 +102,15 @@ public class WorldModel {
 
     /**
      * Set the wind angle in the world model.
-     * @param a Wind angle in degrees. Value is wrapped into the range 0-360.
+     * 
+     * @param a
+     *            Wind angle in degrees. Value is wrapped into the range 0-360.
      */
     public void setWindAngle(float a) {
-        while(a < 0){
+        while (a < 0) {
             a += 360;
         }
-        while(a > 360){
+        while (a > 360) {
             a -= 360;
         }
         windAngle = a;
@@ -97,15 +118,19 @@ public class WorldModel {
 
     /**
      * Get the current wind angle in the world model.
+     * 
      * @return Current wind angle in degrees, in the range 0-360.
      */
     public float getWindAngle() {
         return windAngle;
     }
-    
+
     /**
      * Set the water depth in the world model.
-     * @param d Water depth in meters. Value is truncated into the range 0-{@value #MAX_WATER_DEPTH}.
+     * 
+     * @param d
+     *            Water depth in meters. Value is truncated into the range 0-
+     *            {@value #MAX_WATER_DEPTH}.
      */
     public void setWaterDepth(float d) {
         if (d < 0) {
@@ -116,27 +141,36 @@ public class WorldModel {
         }
         waterDepth = d;
     }
-    
+
     /**
      * Get the current water depth in the world model.
+     * 
      * @return Current water depth in meters.
      */
-    public float getWaterDepth(){
+    public float getWaterDepth() {
         return waterDepth;
     }
-    
-    public float getBoatX(){
+
+    /**
+     * Return the current x-coordinate of the boat.
+     * @return float containing boat's current x-coordinate.
+     */
+    public float getBoatX() {
         return boatX;
     }
-    
-    public float getBoatY(){
+
+    /**
+     * Return the current y-coordinate of the boat.
+     * @return float containing boat's current y-coordinate.
+     */
+    public float getBoatY() {
         return boatY;
     }
 
-    public void step(float dt){
+    public void step(float dt) {
         double ang = Math.toRadians(heading);
         boatX += Math.cos(ang) * boatSpeed * (10 * dt);
         boatY += Math.sin(ang) * boatSpeed * (10 * dt);
     }
-    
+
 }
