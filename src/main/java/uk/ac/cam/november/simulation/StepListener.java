c54304456e92701261c621d531ca589cb2125d90
Jamie Wood <jamie.wood70@ntlworld.com>
2016-02-07 12:41:20 +0000

    Add GraphPanel to DepthPanel

--- src/main/java/uk/ac/cam/november/simulation/StepListener.java
+++ src/main/java/uk/ac/cam/november/simulation/StepListener.java
@@ -2,11 +2,12 @@ package uk.ac.cam.november.simulation;
 
 /**
  * The listener interface for receiving simulation step events.
+ * 
  * @author Jamie Wood
  *
  */
 public interface StepListener {
 
     public void onStep();
-    
+
 }
--- src/main/java/uk/ac/cam/november/simulation/ui/DepthPanel.java
+++ src/main/java/uk/ac/cam/november/simulation/ui/DepthPanel.java
@@ -7,32 +7,38 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import uk.ac.cam.november.simulation.StepListener;
 import uk.ac.cam.november.simulation.WorldModel;
 
-public class DepthPanel extends JPanel {
+public class DepthPanel extends JPanel implements StepListener {
     private static final long serialVersionUID = 2494838642150810963L;
 
     private WorldModel worldModel;
+    private GraphPanel graphPanel;
 
     public DepthPanel(WorldModel worldModel) {
         this.worldModel = worldModel;
         setPreferredSize(new Dimension(250, 100));
+
+        graphPanel = new GraphPanel(250, GraphPanel.FILLED);
+
+        add(graphPanel);
+
+        worldModel.registerStepListener(this);
+    }
+
+    @Override
+    public void onStep() {
+        graphPanel.addValue(worldModel.getWaterDepth());
     }
 
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
         Graphics2D gr = (Graphics2D) g;
 
-        int w = this.getWidth();
-        int h = this.getHeight();
-
-        gr.setColor(Color.GRAY);
-
-        gr.fillRect(0, 0, w, h);
-
-        gr.setColor(Color.WHITE);
-        String angleMessage = "Water Depth: " + worldModel.getWaterDepth();
+        gr.setColor(Color.BLACK);
+        String angleMessage = "Water Depth: " + String.format("%.2f", worldModel.getWaterDepth()) + "m";
         gr.drawString(angleMessage, 8, 16);
-
     }
 }
