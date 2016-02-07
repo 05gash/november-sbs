c8d5e7f801a5d93cf6f165fb83eef3d6655e4207
Jamie Wood <jamie.wood70@ntlworld.com>
2016-02-07 12:41:06 +0000

    Add StepListener to world model

--- src/main/java/uk/ac/cam/november/simulation/WorldModel.java
+++ src/main/java/uk/ac/cam/november/simulation/WorldModel.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.november.simulation;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * This class stores the world state used in the simulation. The values stored
@@ -26,12 +27,36 @@ public class WorldModel {
 
     private DepthMap depthMap;
 
+    private ArrayList<StepListener> listeners;
+
     public WorldModel() {
+        listeners = new ArrayList<StepListener>();
         try {
             depthMap = new DepthMap(800, 600);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Register a class to be called back when the simulation steps.
+     * 
+     * @param sl
+     *            StepListener
+     * @see StepListener
+     */
+    public void registerStepListener(StepListener sl) {
+        listeners.add(sl);
+    }
+
+    /**
+     * Unregister a step listener.
+     * 
+     * @param sl
+     *            The StepListener to remove from the listeners list.
+     */
+    public void unregisterStepListener(StepListener sl) {
+        listeners.remove(sl);
     }
 
     /**
@@ -181,11 +206,21 @@ public class WorldModel {
         return boatY;
     }
 
+    /**
+     * Step the simulation by {@code dt} seconds.
+     * 
+     * @param dt
+     *            The number of seconds to advance time by.
+     */
     public void step(float dt) {
         double ang = Math.toRadians(heading);
         boatX += Math.cos(ang) * boatSpeed * (1 * dt);
         boatY += Math.sin(ang) * boatSpeed * (1 * dt);
         waterDepth = depthMap.getDepth(boatX, boatY);
+        // Notify all listeners
+        for (StepListener sl : listeners) {
+            sl.onStep();
+        }
     }
 
 }
