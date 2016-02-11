4bb03d22e5d2515b99f9beaf40728bcbbc6168b7
Andrej Ivašković <dreian95@gmail.com>
2016-02-11 13:13:39 +0000

    copy on boot

--- src/main/java/uk/ac/cam/november/boot/Boot.java
+++ src/main/java/uk/ac/cam/november/boot/Boot.java
@@ -11,22 +11,60 @@ import uk.ac.cam.november.messages.MessageFormatter;
 import uk.ac.cam.november.messages.SpeechSynthesis;
 import uk.ac.cam.november.simulation.Simulator;
 import uk.ac.cam.november.simulation.network.SimulatorServer;
+import java.nio.chanels.FileChannel;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 class Boot {
 
     public static final int A_LOT_OF_TIME = 1000000000;
 
+    // usual file copy method
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+        FileChannel source = null;
+        FileChannel destination = null;
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        }
+        finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
+    }
+
+    // copy resource into temp folder
+    private static void copyRes(String resname, String fname) {
+        ClassLoader cl = getClass().getClassLoader();
+        try {
+            File fin = new File(cl.getResource("/res/" + resname).getFile());
+            File fout = new File("temp/" + fname);
+            copyFile(fin, fout);
+        } catch (IOException e) {
+            System.err.println("[Error in Boot]");
+            System.err.println(" -- I/O exception raised from copyRes()");
+        }
+    }
+
     public static void main(final String[] args) throws Exception {
 
-	// Making sure that if one thread crashes,
-	// then the whole JVM will shut down.
-	Thread.setDefaultUncaughtExceptionHandler(new Thread.
-   		UncaughtExceptionHandler() {
-   			public void uncaughtException(Thread t, Throwable e) {
-   				System.out.println(t + " throws exception: " + e);
-				System.exit(1);
-  			}
-   		});
+        // Making sure that if one thread crashes,
+        // then the whole JVM will shut down.
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.
+                UncaughtExceptionHandler() {
+                    public void uncaughtException(Thread t, Throwable e) {
+                        System.out.println(t + " throws exception: " + e);
+                        System.exit(1);
+                    }
+                });
 
         boolean runSimServer = false;
 
